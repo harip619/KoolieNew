9fc9536902f2e45574624878bb677e7c539bc854
harip619 <119735773+harip619@users.noreply.github.com>
2024-02-25 10:44:48 +0530

    first commit

---
+++ components/Home/GoogleMapSection.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+function GoogleMapSection() {
+  const containerStyle = {
+    width: '100%',
+    height: '250%'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{mapId:'5e8c679bb6de2190'}}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default GoogleMapSection
