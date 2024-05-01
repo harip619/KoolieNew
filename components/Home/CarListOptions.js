3178a6ec987dadce4f55962083279f0ae07f6fc6
harip619 <119735773+harip619@users.noreply.github.com>
2024-05-01 19:20:55 +0530

    first commit

--- components/Home/CarListOptions.js
+++ components/Home/CarListOptions.js
@@ -4,14 +4,22 @@ import CarListItem from './CarListItem'
 
 function CarListOptions({distance}) {
    const [activeIndex,setActiveIndex]=useState();
+   const [selectedCar,setSelectedCar]=useState([]);
+
   return (
     <div className='mt-5 p-5 overflow-auto h-[250px]'>
       <h2 className='text-[22px] font-bold'>Recommended</h2>
       {CarListData.map((item,index)=>(
-        <div className={`cursor-pointer p-3 rounded-md border  ${activeIndex==index?'border-[2px]':null}`} onClick={()=>setActiveIndex(index)}>
+        <div className={`cursor-pointer p-3 rounded-md border-black  ${activeIndex==index?'border-[2px]':null}`} onClick={()=>{setActiveIndex(index);
+        setSelectedCar(item)}}>
             <CarListItem car={item} distance={distance}/>
         </div>
       ))}
+      {selectedCar?.name?
+      <div className='flex justify-between fixed bottom-5 bg-white p-3 shadow-xl w-full md:w-[30%] border-[1px] items-center rounded-lg'>
+        <h2>Make paymnet for</h2>
+        <button className='p-3 bg-black text-white rounded-md text-center'>Request {selectedCar.name}</button>
+      </div>:null}
     </div>
   )
 }
